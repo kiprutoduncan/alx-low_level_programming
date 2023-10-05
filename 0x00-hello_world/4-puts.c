e5c91037e031bc25eb696c58e0697db590e3efc0
kiprutoduncan <kiprutoduncan36@gmail.com>
2023-10-05 08:51:41 -0700

    hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,10 @@
 /**
  * main -this is the main function
  *
- *Return :always returns 0
+ *Return :always 0
  */
-int main(void){
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
