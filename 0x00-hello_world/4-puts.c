cf49d0f7453a5916c718376b9870f84ec5361230
kiprutoduncan <kiprutoduncan36@gmail.com>
2023-10-05 08:55:19 -0700

    hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- *main -this is the main function
- *
- *Return :always 0
+ * main - main block
+ * Return: 0
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
